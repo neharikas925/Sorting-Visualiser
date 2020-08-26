a4abb8e6dfb735feb70f1b5e9e8dba3f8c9d13dd
neharikas925 <neharikas926@gmail.com>
2020-08-26 15:16:29 +0530

    Changed navbar items alignment

--- sorting-visualizer-master/src/components/Home.vue
+++ sorting-visualizer-master/src/components/Home.vue
@@ -1,49 +1,51 @@
 <template>
   <div class="main">
 
-    <v-app-bar style="height: 80px;">
-        <v-toolbar-title style="font-size: 40px; font-weight: bold; padding-top: 20px; color: #33cabb" >Sorting Visualiser</v-toolbar-title>
+    <v-app-bar>
+        <v-toolbar-title style="font-size: 20px;" color="white">Sorting Algo Visualiser </v-toolbar-title>
         
 
           <v-spacer></v-spacer>
           
-
-              <v-slider style="padding-top: 50px;" 
+          <v-row style="padding-top: 30px;">
+            <v-col cols="md">
+              <v-slider
                     label="Number" v-model="number"
-                    :thumb-size="24" thumb-label="always" 
+                    :thumb-size="19" thumb-label="always" 
                     :disabled="isSorting"
-                    min="10" max="50"
+                    min="20" max="50"
                     @change="generate">
               </v-slider>
-              
-              <v-slider style="padding-top: 50px;" 
+              </v-col>
+              <v-col cols="md">
+              <v-slider
                         label="Speed" v-model="speed"
-                        :thumb-size="24" thumb-label="always" 
+                        :thumb-size="19" thumb-label="always" 
                         tick-size= "20"
-                        min="1" max="12">
-              </v-slider>
-
+                        min="1" max="10">
+              </v-slider></v-col>
+          </v-row>
           
 
           <div class="button-container">
 
-              <v-btn class="button white--text" color="#33bdef"
+              <v-btn small  rounded  class="button white--text" color="#33bdef"
                       v-on:click="shuffle" 
                       :disabled="isSorting" >Shuffle!</v-btn>
-              <v-btn class="button white--text" color="#33bdef"
+              <v-btn small  rounded  class="button white--text" color="#33bdef"
                       v-on:click="sort(selectedAlgo)" 
                       :disabled="isSorting">Sort!</v-btn>
                       
           </div>
           
-          
+
             <v-select :items="sortAlgorithms" 
                       item-text="name"
                       item-value="id"
                       v-model="selectedAlgo"
                       :disabled="isSorting"
                       label="Algorithm" outlined
-                      style="padding-top: 50px;" right>
+                      style="padding-top: 30px; width: 0px" right>
             </v-select>
           
         </v-app-bar>
@@ -53,9 +55,9 @@
 
         <transition-group tag="div" name="list-animation" class="array-to-be-sorted">
 
-           <div v-for="num in array" v-bind:key="num" 
+            <div v-for="num in array" v-bind:key="num" 
                     v-bind:class="['list-animation-item', 'list-item' ]"
-                        v-bind:style="{ height: 6.0*num + 'px' }">
+                        v-bind:style="{ height: 4.8*num*100/number + 'px' }">
             </div>
             
         </transition-group>
@@ -65,22 +67,18 @@
   </div>
 </template>
 
+
 <script>
 export default {
   name: 'Home',
-
   data() {
     return {
         array: [],
         isManipulatingArray: false,
-
         selectedAlgo: "1",
-
         number: "20",
         speed: "5",
-
         isSorting: false,
-
         styleObject: {
           height: "200px",
         },
@@ -114,21 +112,17 @@ export default {
         else if(algo=="2") this.selectionSort(this.array);
         else if(algo=="3") this.insertionSort(this.array);
       },
-
       sleep: function() {
         return new Promise(resolve => setTimeout(resolve, (100*5)/this.speed));
       },
-
       arraySetWithoutIndexes: async function(array, index, value) {
           array.splice(index, 1, value);
       },
-
       arraySwap: async function(array, indexA, indexB) {
         var x = array[indexA];
         this.arraySetWithoutIndexes(array, indexA, array[indexB]);
         this.arraySetWithoutIndexes(array, indexB, x);
       },
-
       shuffle: function() {
         this.isSorting = false;
         var a = this.array;
@@ -138,7 +132,6 @@ export default {
             this.arraySwap(a, i, j);
         }
       },
-
       bubbleSort: async function(a) {
         var len = a.length;
         for (var i = len - 1; i >= 0; i--) {
@@ -151,10 +144,8 @@ export default {
         }
         this.isSorting = false;
       },
-
       selectionSort: async function(a) {
         var minIdx, len = a.length;
-
         for (var i = 0; i < len; i++) {
             minIdx = i;
             for (var j = i + 1; j < len; j++) {
@@ -167,14 +158,11 @@ export default {
         }
         this.isSorting = false;
       },
-
       insertionSort: async function(a) {
         var i, len = a.length, el, j;
-
         for (i = 1; i < len; i++) {
             el = a[i];
             j = i;
-
             while (j > 0 && a[j - 1] > el) {
                 a[j] = a[j - 1];
                 j--;
@@ -189,7 +177,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 input:focus,
 select:focus,
 textarea:focus,
@@ -201,7 +188,6 @@ button:focus {
     height:800px;
     background-color: #1fed89;
     background-image: linear-gradient(160deg, #1fed89 0%, #12d6e7 50%, #341bdf 100%);
-
 ;
 }
 .hello {
@@ -209,14 +195,12 @@ button:focus {
   overflow: hidden;
   position: relative;
 }
-
 .array-to-be-sorted {
     display: flex;
     position: relative;
     justify-content: center;
     align-items: center;
 }
-
 .list-item {
     display: flex;
     position: relative;
@@ -235,7 +219,6 @@ button:focus {
    
    
     
-
     &.active {
       background: #92cc6b;
     }
@@ -246,7 +229,6 @@ button:focus {
       background: #ad6bcc;
     }
 }
-
 .list-animation-item {
     transition: all ease 0.5s;
 }
@@ -257,11 +239,9 @@ button:focus {
 .button {
   margin: 5px 15px;
 }
-
 .button:hover {
     background-color: #33cabb;
 }
-
 .button:active {
     position: relative;
     top: 1px;
